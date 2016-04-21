37d4ce257c8be889d92f36d6c0dbbf2036360921
princessareum <Princess@A-Reums-MacBook-Pro.local>
2016-04-20 20:27:29 -0600

    done

--- server.js
+++ server.js
@@ -1,1 +1,56 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var messages = [];
+var app = express();
 
+app.use(bodyParser.json());
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+
+app.post('/', function( req, res ) {
+
+    messages.push({
+      message: req.body.message,
+      time: new Date()
+    });
+
+    res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+
+});
+
+
+app.options('/', function(req, res){
+  res. status(200).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }).send(JSON.stringify(messages));
+});
+
+
+
+
+
+
+app.listen(7000, function(){
+  console.log('listening on port 7000')
+});
